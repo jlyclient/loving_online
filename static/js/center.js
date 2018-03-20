c06383642de23f311bc5e7c7be92cbea40702aed
jlyclient <304696737@qq.com>
2018-03-20 23:58:41 +0800

    修改

--- static/js/center.js
+++ static/js/center.js
@@ -64,39 +64,38 @@ $(function() {
 
                 // 其他资料
                 var love_material = '';
-                love_material += '<div class="love_col love_col_4">月薪：'+ salary[centerobj.user.salary] +'</div>'+
-                '<div class="love_col love_col_4">职业：'+ work[centerobj.user.work]+'</div>'+
-                '<div class="love_col love_col_4">购车：'+ house[centerobj.user.car] +'</div>'+
-                '<div class="love_col love_col_4">购房：'+ house[centerobj.user.house] +'</div>';
+                love_material += '<div class="love_col love_col_4">月薪：'+ salary[centerobj.otherinfo.salary] +'</div>'+
+                '<div class="love_col love_col_4">职业：'+ work[centerobj.otherinfo.work]+'</div>'+
+                '<div class="love_col love_col_4">购车：'+ house[centerobj.otherinfo.car] +'</div>'+
+                '<div class="love_col love_col_4">购房：'+ house[centerobj.otherinfo.house] +'</div>';
                 $("#love_material").append(love_material);
                     // 其他资料账号相关
                 var love_account = '';
-                love_account += '<div class="love_col love_col_5">'+
-                '手机：' + centerobj.otherinfo.mobile +
-                '<div class="love_other_tools">'+
-                    '<button class="btn_center">'+ centerobj.otherinfo.public_m === 0 ? '对外隐藏' : '对外公开' +'</button>'+
+                love_account += '<div class="love_col love_col_5">手机：' + centerobj.otherinfo.mobile +
+                    '<div class="love_other_tools">'+
+                        '<button class="btn_center">'+ centerobj.otherinfo.public_m === 0 ? '对外隐藏' : '对外公开' +'</button>'+
+                    '</div>'+
+                '</div>'+
+                '<div class="love_col love_col_5">'+
+                    '邮箱：'+ centerobj.otherinfo.email +
+                    '<div class="love_other_tools">'+
+                        '<button class="btn_center">'+ centerobj.otherinfo.public_e === 0 ? '对外隐藏' : '对外公开' +'</button>'+
+                        '<button class="btn_center btn_center_plain">'+ centerobj.otherinfo.verify_e === 0 ? '验证邮箱' : '解绑1' +'</button>'+
                 '</div>'+
                 '</div>'+
                 '<div class="love_col love_col_5">'+
-                '邮箱：'+ centerobj.otherinfo.email +
-                '<div class="love_other_tools">'+
-                    '<button class="btn_center">'+ centerobj.otherinfo.public_e === 0 ? '对外隐藏' : '对外公开' +'</button>'+
-                    '<button class="btn_center btn_center_plain">'+ centerobj.otherinfo.verify_e === 0 ? '验证邮箱' : '解绑1' +'</button>'+
-                '</div>'+
-                '</div>'+
-                '<div class="love_col love_col_5">'+
-                '微信：'+ centerobj.otherinfo.wx +
-                '<div class="love_other_tools">'+
-                    '<button class="btn_center">'+ centerobj.otherinfo.public_w === 0 ? '对外隐藏' : '对外公开' +'</button>'+
-                    '<button class="btn_center">'+ centerobj.otherinfo.verify_w === 0 ? '验证微信' : '解绑2' +'</button>'+
-                '</div>'+
+                    '微信：'+ centerobj.otherinfo.wx +
+                    '<div class="love_other_tools">'+
+                        '<button class="btn_center">'+ centerobj.otherinfo.public_w === 0 ? '对外隐藏' : '对外公开' +'</button>'+
+                        '<button class="btn_center">'+ centerobj.otherinfo.verify_w === 0 ? '验证微信' : '解绑2' +'</button>'+
+                    '</div>'+
                 '</div>'+
                 '<div class="love_col love_col_5">'+
-                'QQ：'+ centerobj.otherinfo.qq +
-                '<div class="love_other_tools">'+
-                    '<button class="btn_center">'+ centerobj.otherinfo.public_q === 0 ? '对外隐藏' : '对外公开' +'</button>'+
-                    '<button class="btn_center btn_center_plain">'+ centerobj.otherinfo.verify_q === 0 ? '验证qq' : '解绑3' +'</button>'+
-                '</div>'+
+                    'QQ：'+ centerobj.otherinfo.qq +
+                    '<div class="love_other_tools">'+
+                        '<button class="btn_center">'+ centerobj.otherinfo.public_q === 0 ? '对外隐藏' : '对外公开' +'</button>'+
+                        '<button class="btn_center btn_center_plain">'+ centerobj.otherinfo.verify_q === 0 ? '验证qq' : '解绑3' +'</button>'+
+                    '</div>'+
                 '</div>';
                 $("#love_account").append(love_account);
 
